9452a72b2f3b7c1cb6f532e1c3afd3dc786566b2
AndrewCTer <33009070+AndrewTer@users.noreply.github.com>
2019-03-26 23:31:52 +0300

    Add files via upload
    #Простой свап
    Реализуйте метод boolean swap(int i, int j, int[] anArray), меняющий местами элементы по индексам i и j в массиве anArray и возвращающий false, если индексы выходят за пределы массива

---
+++ lab1/Lab1.java
@@ -0,0 +1,44 @@
+package lab1;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author terehin.andrey
+ * 
+ * Задание:
+ * Реализуйте метод boolean swap(int i, int j, int[] anArray), меняющий местами элементы
+ * по индексам i и j в массиве anArray и возвращающий false, если индексы выходят за
+ * пределы массива
+ */
+
+public class Lab1 
+{
+    public static boolean swap(int i, int j, int[] anArray) 
+    {
+        int maxIndex = anArray.length - 1;
+
+        if ((i < 0) || (j < 0) || (i > maxIndex) || (j > maxIndex)) 
+        {
+            return false;
+        } 
+        else 
+        {
+            int tmp = anArray[i];
+            anArray[i] = anArray[j];
+            anArray[j] = tmp;
+            
+            System.out.println("После: "+Arrays.toString(anArray));
+            return true;
+        }
+    }
+    
+    public static void main(String[] args) 
+    {
+        int[] myArray = {11, 12, 13, 14, 15};
+        int i=1, j=2;
+        System.out.println("До: "+Arrays.toString(myArray));
+        swap(i, j, myArray);
+    }
+    
+}
